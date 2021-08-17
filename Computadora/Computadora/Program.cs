73836eddf9398e087b733fcf1d404afbf2b8b6b3
LudwinLara21 <87671764+LudwinLara21@users.noreply.github.com>
2021-08-17 13:10:26 -0600

    Ultima Actualizacion

--- Computadora/Computadora/Program.cs
+++ Computadora/Computadora/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Computadora.Entidades;
+using Computadora.Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,15 @@ namespace Computadora
     {
         static void Main(string[] args)
         {
+            ClsComputadora clscomputadora = new ClsComputadora();
+            NClsComputadora nclscomputadora = new NClsComputadora();
+
+            clscomputadora.MemoriaRAM ="16";
+            clscomputadora.Procesador = "i7";
+
+            Console.WriteLine(nclscomputadora.Encender(clscomputadora));
+
+            Console.ReadLine();
         }
     }
 }
