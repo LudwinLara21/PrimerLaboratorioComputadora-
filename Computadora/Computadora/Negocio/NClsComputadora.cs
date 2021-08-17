7ec9cf8eb186a53bf1a3e2c77a55bf1faa75382f
1Ramirez10 <88685961+1Ramirez10@users.noreply.github.com>
2021-08-17 13:03:09 -0600

    Actualizacion

--- Computadora/Computadora/Negocio/NClsComputadora.cs
+++ Computadora/Computadora/Negocio/NClsComputadora.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Computadora.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,37 @@ namespace Computadora.Negocio
 {
     class NClsComputadora
     {
+
+
+
+        public String Apagar(ClsComputadora computadora)
+        {
+            return "La computadora con procesador" + computadora.Procesador + "Se esta apagando ";
+
+        }
+
+        public String monitor(ClsComputadora computadora)
+        {
+            return "El monitor " + computadora.Monitor + "Esta dañado ";
+
+        }
+
+        public String teclado(ClsComputadora computadora)
+        {
+            return "El teclado " + computadora.Teclado + "esta fallando ";
+
+        }
+
+        public String fuente(ClsComputadora computadora)
+        {
+            return "La fuente   " + computadora.FuenteDePoder + "No esta conectado ";
+        }
+
+        public String Encender(ClsComputadora computadora)
+        {
+            return "La computadora con memoria RAM de " + computadora.MemoriaRAM + " y micro procesador " + computadora.Procesador + " esta encendida";
+
+        }
     }
 }
+
