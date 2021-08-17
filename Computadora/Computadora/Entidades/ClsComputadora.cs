8909cba372c7700edda7e645cfd94c3de30e2320
LudwinLara21 <87671764+LudwinLara21@users.noreply.github.com>
2021-08-17 12:47:53 -0600

    Actualizacion

--- Computadora/Computadora/Entidades/ClsComputadora.cs
+++ Computadora/Computadora/Entidades/ClsComputadora.cs
@@ -8,5 +8,26 @@ namespace Computadora.Entidades
 {
     class ClsComputadora
     {
+        private String discoDuro;
+        private String mouse;
+        private String teclado;
+        private String monitor;
+        private String tarjetaMadre;
+        private String fuenteDePoder;
+        private String procesador;
+        private String memoriaRAM;
+        private String tarjetaGrafica;
+        private String cpu;
+
+        public string DiscoDuro { get => discoDuro; set => discoDuro = value; }
+        public string Mouse { get => mouse; set => mouse = value; }
+        public string Teclado { get => teclado; set => teclado = value; }
+        public string Monitor { get => monitor; set => monitor = value; }
+        public string TarjetaMadre { get => tarjetaMadre; set => tarjetaMadre = value; }
+        public string FuenteDePoder { get => fuenteDePoder; set => fuenteDePoder = value; }
+        public string Procesador { get => procesador; set => procesador = value; }
+        public string MemoriaRAM { get => memoriaRAM; set => memoriaRAM = value; }
+        public string TarjetaGrafica { get => tarjetaGrafica; set => tarjetaGrafica = value; }
+        public string CPU { get => cpu; set => cpu = value; }
     }
 }
